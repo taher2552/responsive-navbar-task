3eb6e026da256615b389c6310e5814042054d49d
taher2552 <stardom.taher@gmail.com>
2022-12-04 18:15:51 +0530

    changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const hamburger=document.querySelector('.fa-bars');
 const responsive=document.querySelector('.responsive')
+const itag=document.querySelectorAll('.fa');
 
 hamburger.addEventListener('click',()=>{
     responsive.style.display="block";
 })
+
+
+
+
